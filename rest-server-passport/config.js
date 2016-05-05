1aabf2fa56c2f35aaf458908da733fc0bc14684d
Nick Fabrizio <nfabrizio@medianewsgroup.com>
2016-05-05 11:54:46 -0600

    Adds Oauth authentication through Facebook

--- rest-server-passport/config.js
+++ rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl': 'mongodb://localhost:27017/conFusion'
+  'mongoUrl': 'mongodb://localhost:27017/conFusion',
+  'facebook': {
+    clientID: '781326628670934',
+    clientSecret: '5f100249522e278b2566bc9f42eedca8',
+    callbackURL: 'https://localhost:3443/users/facebook/callback'
+  }
 }
